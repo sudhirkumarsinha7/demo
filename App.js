31f8bb6c6ba55c4c503241dd95d9db4ce082af2c
Sudhir Kumar <sudhirkumarsinha7@gmail.com>
2022-08-26 11:31:35 +0530

    git test

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Routes from './src/Screens/Routes';
 import 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import Store from './src/Store';
-///git test test
+///git test test crew
 export default class App extends React.PureComponent {
   render() {
     return (
